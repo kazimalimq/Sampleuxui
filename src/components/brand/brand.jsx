d897023011736475ada2accb04ca4f8081726165
Ali Kazim <ali.kazim@students.mq.edu.au>
2021-12-04 01:53:41 +0500

    Added Brands Section

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './brand.css'
 
+import {google, shopify, slack, atlassion, dropbox } from './imports'
+
 const Brand = () => {
     return (
+        <div className=" gpt3__brand section__padding">
         <div>
-            brand
+            <img src = {google} alt = "google"/>  
+        </div>
+        <div>
+            <img src = {shopify} alt = "shopify"/>  
+        </div>
+        <div>
+            <img src = {slack} alt = "slack"/>  
+        </div>
+        <div>
+            <img src = {atlassion} alt = "atlassion"/>  
+        </div>
+        <div>
+            <img src = {dropbox} alt = "dropbox"/>  
+        </div>               
         </div>
     )
 }
